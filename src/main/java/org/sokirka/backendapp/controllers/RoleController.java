2321703f9f2843c6292291af3335f873d4ca95ea
Eugine Sokirka <sokyrka_evgeniy@yahoo.com>
2017-03-06 17:19:18 +0200

    Controllers have started use service layer.

--- src/main/java/org/sokirka/backendapp/controllers/RoleController.java
+++ src/main/java/org/sokirka/backendapp/controllers/RoleController.java
@@ -1,7 +1,7 @@
 package org.sokirka.backendapp.controllers;
 
-import org.sokirka.backendapp.dao.RoleDao;
 import org.sokirka.backendapp.entities.Role;
+import org.sokirka.backendapp.services.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -14,28 +14,28 @@ import org.springframework.web.bind.annotation.*;
 public class RoleController {
 
     @Autowired
-    private RoleDao roleDao;
+    private RoleService roleService;
 
     @RequestMapping(method = RequestMethod.GET)
     public Role getRole(@PathVariable("id") String id) {
-        return roleDao.findOne(Long.parseLong(id));
+        return roleService.getById(Long.parseLong(id));
     }
 
     @RequestMapping(method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.OK)
     public void newRole(@RequestBody Role role) {
-        roleDao.save(role);
+        roleService.save(role);
     }
 
     @RequestMapping(method = RequestMethod.PUT)
     @ResponseStatus(HttpStatus.OK)
     public void updateRole(@RequestBody Role role) {
-        roleDao.save(role);
+        roleService.update(role);
     }
 
     @RequestMapping(method = RequestMethod.DELETE)
     @ResponseStatus(HttpStatus.OK)
     public void deleteRole(@RequestBody Role role) {
-        roleDao.delete(role);
+        roleService.delete(role);
     }
 }
--- src/main/java/org/sokirka/backendapp/controllers/UserController.java
+++ src/main/java/org/sokirka/backendapp/controllers/UserController.java
@@ -1,7 +1,7 @@
 package org.sokirka.backendapp.controllers;
 
-import org.sokirka.backendapp.dao.UserDao;
 import org.sokirka.backendapp.entities.User;
+import org.sokirka.backendapp.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -14,28 +14,28 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     @Autowired
-    private UserDao userDao;
+    private UserService userService;
 
     @RequestMapping(method = RequestMethod.GET)
     public User getUser(@PathVariable("id") String id) {
-        return userDao.findOne(Long.parseLong(id));
+        return userService.getById(Long.parseLong(id));
     }
 
     @RequestMapping(method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.OK)
     public void newUser(@RequestBody User user) {
-        userDao.save(user);
+        userService.save(user);
     }
 
     @RequestMapping(method = RequestMethod.PUT)
     @ResponseStatus(HttpStatus.OK)
     public void updateUser(@RequestBody User user) {
-        userDao.save(user);
+        userService.update(user);
     }
 
     @RequestMapping(method = RequestMethod.DELETE)
     @ResponseStatus(HttpStatus.OK)
     public void deleteUser(@RequestBody User user) {
-        userDao.delete(user);
+        userService.delete(user);
     }
 }
